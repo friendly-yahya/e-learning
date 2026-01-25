f78156b7065d3dcde1a8ebd4cf26d9c970a04a4d
friendly-yahya <yahyafarkli63@gmail.com>
2026-01-25 18:33:16 +0100

    Makes header.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu"
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <main className="flex min-h-screen w-full container flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -12,6 +21,38 @@ export default function Home() {
           height={20}
           priority
         />
+        <div className="flex flex-row items-center">
+          <div className="flex flex-row items-center justify-between content-between" >
+            <h1 className="text-5xl font-semibold" >Echo</h1>
+            <NavigationMenu>
+              <NavigationMenuList>
+                <NavigationMenuItem>
+                   <Button variant="ghost" className="rounded-full">Home</Button>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                  <Button variant="ghost" className="rounded-full">Analytics</Button>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                  
+                  
+                    <NavigationMenuTrigger className="rounded-full">Browse
+                    </NavigationMenuTrigger>
+                    <NavigationMenuContent className="**left-auto right-0**">
+                      <NavigationMenuLink>Link</NavigationMenuLink>
+                      <NavigationMenuLink>Link</NavigationMenuLink>
+                      <NavigationMenuLink>Link</NavigationMenuLink>
+                      <NavigationMenuLink>Link</NavigationMenuLink>
+                    </NavigationMenuContent>
+                    
+
+                </NavigationMenuItem>
+              </NavigationMenuList>
+            </NavigationMenu>
+          </div>
+          <div>
+
+          </div>
+        </div>
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             yoooooooo it's the ball wizard
